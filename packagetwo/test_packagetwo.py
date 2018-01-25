d3c22fdce0a8760d84a5e20639c1dcec120f2961
Neal Tanner <neal@tntAnalysis.com>
2018-01-25 09:58:32 -0600

    automated test discovery

---
+++ packagetwo/test_packagetwo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 20:44:07 2018
+
+@author: tanner
+"""
+
+import unittest
+
+class TestPackageTwo(unittest.TestCase):
+    """ A TestCase (class) is used to group similar tests """
+
+    def setUp(self):
+        """ setUp gets called before each individual test method. """
+        pass
+
+    def tearDown(self):
+        """ tearDown gets called after each individual test method. """
+        pass
+
+    def test_instantiation(self):
+        """ Can we even instantiate this thing? """
+
+        print('Test Instantiation')
+
+
+#################################
+### Module Level Test Harness ###
+#################################
+if __name__ == "__main__":
+
+    # Kick off all unit tests contained in the file
+    unittest.main(verbosity=2)
